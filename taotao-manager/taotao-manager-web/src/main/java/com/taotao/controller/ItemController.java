1f8685076b09232646e01c2bcb313a4001ef7ee9
dengweida <45987321+dengweida@users.noreply.github.com>
2019-07-28 17:37:31 +0800

    test

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
@@ -31,5 +33,10 @@ public class ItemController {
 		return result;
 	}
 	
-	
+	@RequestMapping(value="/item/save", method=RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult createItem(TbItem item, String desc) {
+		TaotaoResult result = itemService.createItem(item, desc);
+		return result;
+	}
 }
