a0622ecf65bbb7c22b0b7f98ccade1ab08bf3f63
zoushiming <zoushiming@zoushiming-PC>
2019-08-12 18:18:01 +0800

    test

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
@@ -1,10 +1,12 @@
 package com.taotao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.utils.HttpClientUtil;
 import com.taotao.pojo.TaotaoResult;
 import com.taotao.pojo.TbContent;
 import com.taotao.service.ContentService;
@@ -14,11 +16,16 @@ import com.taotao.service.ContentService;
 public class ContentController {
 	@Autowired
 	private ContentService contentService;
-	
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
 	@RequestMapping("/save")
 	@ResponseBody
 	public TaotaoResult insertContent(TbContent content) {
 		TaotaoResult result = contentService.insertContent(content);
+		//调用taotao-rest发布的服务，同步缓存
+	  HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
 		return result;
 	}
 }
